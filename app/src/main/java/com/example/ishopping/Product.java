d1ce84435cec682634d9456edb8d6f3f613ae6a0
magog <sdgijhdbfsabc@gmail.com>
2020-11-20 18:06:25 +0200

    product class added

---
+++ app/src/main/java/com/example/ishopping/Product.java
@@ -0,0 +1,71 @@
+package com.example.ishopping;
+
+import android.media.Image;
+
+public class Product {
+
+    private String productId;
+    private String productName;
+    private String productQuantity;
+    private String productCategory;
+    private String productDescription;
+    private String productImage;
+
+
+    public Product(String productId, String productName, String productQuantity, String productCategory, String productDescription, String productImage) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productQuantity = productQuantity;
+        this.productCategory = productCategory;
+        this.productDescription = productDescription;
+        this.productImage = productImage;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(String productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+}
